8658d58aeb3becf963ccc47435cdb9f61eb5403e
i3nriq <kikemaxmq@hotmail.com>
2013-08-27 20:03:58 -0600

    Gestor de Base de Datos
    Panel de navegacion

--- GBD - CCS/SBD - CCS/Inicio.cs
+++ GBD - CCS/SBD - CCS/Inicio.cs
@@ -18,6 +18,10 @@ namespace SBD___CCS
     public partial class Inicio : Form
     {
 
+        int inControl_tabla_multi = 0;
+        int inControl_b = 0;
+        int inControl_t = 0;
+
         public String stHost = "";
         public String stBase_de_datos = "";
         public String stUsuario = "";
@@ -37,6 +41,7 @@ namespace SBD___CCS
             InitializeComponent();
             lt_Multi_funcion.SetSelected(0, true);
             tm_Estado_BD.Start();
+            CargarBasesDeDatos(0);
         }
 
         private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
@@ -92,22 +97,27 @@ namespace SBD___CCS
 
         private void btn_SELECCIONARBD_Click(object sender, EventArgs e)
         {
-
+            IniciarGestor();
         }
 
         private void lista_multi_DoubleClick(object sender, EventArgs e)
         {
-
+            IniciarGestor();
         }
 
         private void btn_INICIOBD_Click(object sender, EventArgs e)
         {
-
+            this.lb_BD.Visible = false;
+            this.cb_BD.Visible = false;
+            this.bt_Seleccionar_BD.Visible = true;
+            inControl_tabla_multi = 0;
+            CargarBasesDeDatos(0);
         }
 
         private void cb_BD_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            stBase_de_datos = cb_BD.Text;
+            CargarTablas();
         }
 
 
@@ -189,7 +199,162 @@ namespace SBD___CCS
 
         }
 
+        /***************************************************************
+        NOMBRE:             CargarBasesDeDatos
+        FECHA:		    24-08-2013
+        CREADOR:     	    Zayda Hernandez          
+        DESCRIPCIÓN         Es utilizado para cargar las Bases de Datos a diferentes componentes
+        DETALLE:            Verifica que componente se desea actualizar. multiusos
+        MODIFICACIÓN:       
+        ***************************************************************/
+        public void CargarBasesDeDatos(int inC)
+        {
 
+            string stCadena = "show databases;";
+            try
+            {
+                CSQL.CONECTAR(stHost, stBase_de_datos, stUsuario, stContrasena);
+                CSQL.conectarSQL.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = stCadena;
+                string[] vector = stCadena.Split(' ');
+                cmd.Connection = CSQL.conectarSQL;
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (inC == 0)
+                {
+                    lt_Multi_funcion.Items.Clear();
+                }
+                if (inC == 1)
+                {
+                    cb_BD.Items.Clear();
+                }
+
+                while (reader.Read())
+                {
+                    if (inC == 0)
+                    {
+                        lt_Multi_funcion.Items.Add(reader.GetString(0));
+                    }
+                    if (inC == 1)
+                    {
+                        cb_BD.Items.Add(reader.GetString(0));
+                    }
+                    inControl_b = 1;
+                }
+                if (inC == 0)
+                {
+                    if (inControl_b == 1)
+                    {
+                        lt_Multi_funcion.SetSelected(0, true);
+                    }
+                    else
+                    {
+                        lt_Multi_funcion.Items.Add("No existen Bases de Datos");
+                        inControl_b = 0;
+                    }
+                }
+
+            }
+
+            catch (MySqlException ex)
+            {
+                tb_Resultados.Text = ("Error de sintaxis" + "\n" + ex);
+                CSQL.conectarSQL.Close();
+            }
+
+            CSQL.conectarSQL.Close();
+        }
+
+        /***************************************************************
+        NOMBRE:             CargarTablas          
+        FECHA:		    24-08-2013
+        CREADOR:     	    Zayda Hernandez          
+        DESCRIPCIÓN         Carga el listado de la base de datos seleccionada
+        DETALLE:            Se despliega en un componente espefico
+        MODIFICACIÓN:       
+        ***************************************************************/
+        public void CargarTablas()
+        {
+
+            string stCadena = "show tables;";
+            try
+            {
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = stCadena;
+                string[] vector = stCadena.Split(' ');
+
+                CSQL.CONECTAR(stHost, stBase_de_datos, stUsuario, stContrasena);
+                CSQL.conectarSQL.Open();
+                cmd.Connection = CSQL.conectarSQL;
+
+                if ((vector[0] == "show"))
+                {
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    lt_Multi_funcion.Items.Clear();
+
+                    while (reader.Read())
+                    {
+                        lt_Multi_funcion.Items.Add(reader.GetString(0));
+                        inControl_t = 1;
+                    }
+                    if (inControl_t == 1)
+                    {
+                        lt_Multi_funcion.SetSelected(0, true);
+                        inControl_t = 0;
+                    }
+                    else
+                    {
+                        lt_Multi_funcion.Items.Add("No existen Tablas");
+                        inControl_t = 0;
+                    }
+
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                tb_Resultados.Text = ("Error de sintaxis" + "\n" + ex);
+                CSQL.conectarSQL.Close();
+            }
+
+            CSQL.conectarSQL.Close();
+        }
+
+
+
+        /***************************************************************
+        NOMBRE:             IniciarGestor
+        FECHA:		    26-08-2013
+        CREADOR:     	    Zayda Hernandez      
+        DESCRIPCIÓN         Inicia el Gestor de 
+        DETALLE:            Al primer click BD y al segundo Tablas
+        MODIFICACIÓN:       
+        ***************************************************************/
+        public void IniciarGestor()
+        {
+            if ((inControl_tabla_multi == 0) && (inControl_b == 1))
+            {
+
+                String stValor_tabla = lt_Multi_funcion.SelectedItem.ToString();
+                stBase_de_datos = ""; stBase_de_datos = stBase_de_datos + stValor_tabla;
+                inControl_tabla_multi = 1;
+                this.lb_BD.Visible = true;
+                this.cb_BD.Visible = true;
+                this.bt_Seleccionar_BD.Visible = false;
+                lt_Multi_funcion.Items.Clear();
+                CargarBasesDeDatos(1);
+                CargarTablas();
+            }
+            if (inControl_tabla_multi == 1)// Es una tabla
+            {
+
+
+            }
+        }
 
 
 
